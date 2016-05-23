4163933b4a7ac1636cc1f72555d34d9820e3bcc9
patricknugent <nugget84@gmail.com>
2016-05-22 19:04:51 -0700

    Added Menus
    Rudimentary main menu and in game pause menu.

--- Assets/Scripts/Game controller/GameController.cs
+++ Assets/Scripts/Game controller/GameController.cs
@@ -3,9 +3,40 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
-	void Update()
+
+    private Canvas canvasComponent;
+
+    void Start()
+    {
+        canvasComponent = GameObject.Find("Pause canvas").GetComponent<Canvas>();
+        canvasComponent.enabled = false;
+    }
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
+
+            if (Time.timeScale != 0)
+            {
+                Time.timeScale = 0;
+                canvasComponent.enabled = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                canvasComponent.enabled = false;
+            }
+
             Application.Quit();
 	}
+
+    public void LoadLevel(string LevelToLoad)
+    {
+        Time.timeScale = 1;
+        Application.LoadLevel(LevelToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }

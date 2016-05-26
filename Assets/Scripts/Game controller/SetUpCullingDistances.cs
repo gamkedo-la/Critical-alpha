fe8185cd3aeaf48f96be3c66fa2dd81e75e63dab
TimWaskett <tim.waskett@gmail.com>
2016-05-26 23:00:08 +0100

    Added a few placeholder prefabs to get an idea of scale, plus added layers so different sized objects can be culled at different distances

---
+++ Assets/Scripts/Game controller/SetUpCullingDistances.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetUpCullingDistances : MonoBehaviour
+{
+	void Start() 
+	{
+		var camera = Camera.main;
+		float[] distances = new float[32];
+
+		distances[9] = 500;			// Small object
+		distances[10] = 1000;		// Medium object
+		distances[11] = 1500;		// Large object
+
+		camera.layerCullDistances = distances;
+		camera.layerCullSpherical = true;
+	}
+}

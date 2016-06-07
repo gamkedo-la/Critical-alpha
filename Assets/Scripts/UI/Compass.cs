220fc789eeb615414a0d21412e09cf15d6ad26d4
patricknugent <nugget84@gmail.com>
2016-06-07 00:29:40 -0700

    Added

---
+++ Assets/Scripts/UI/Compass.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour {
+
+    private RawImage compassRawImage;
+    private Transform playerTransform;
+    private float offsetX;
+
+
+
+	// Use this for initialization
+	void Start () {
+        compassRawImage = GetComponent<RawImage>();
+        playerTransform = FindObjectOfType<PlayerFlyingInput>().transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Direction: " + playerTransform.transform.eulerAngles.y);
+
+        offsetX = playerTransform.transform.eulerAngles.y;
+
+        //Divide by 360 because uvRect range is from 0 to 1
+        offsetX /= 360;
+
+        //Keep all the values the same except for X
+        compassRawImage.uvRect = new Rect(offsetX, compassRawImage.uvRect.y, compassRawImage.uvRect.width, compassRawImage.uvRect.height);
+
+
+
+    }
+}

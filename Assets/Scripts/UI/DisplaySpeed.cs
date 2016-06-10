9de0e7e2dcd55ca2f809c084750297653beed49d
patricknugent <nugget84@gmail.com>
2016-06-10 14:03:45 -0700

    Moved Speedometer to HUD, added outlines
    -Speedometer is now on the HUD, used same Slider code just changed the
    visual aspects of it.
    -Added a speed multiplier to Speed Text to allow conversion to more
    realistic units such as mph or kph
    -Added grey outlines to all HUD elements, everything set to 128
    including the alpha: R 128 G 128 B 128 A 128.  Right now this is being
    done by a script, if we like it we can add it to the graphics
    themselves.

---
+++ Assets/Scripts/UI/DisplaySpeed.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplaySpeed : MonoBehaviour {
+
+    private Slider speedSlider;
+    private Text speedText;
+
+    //Convert speed to more useful unit, like mph or kph
+    public float speedMultiplier;
+
+	// Use this for initialization
+	void Start () {
+
+        speedSlider = GameObject.Find("Speed Slider").GetComponent<Slider>();
+        speedText = GetComponent<Text>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        speedText.text = Mathf.Ceil(speedSlider.value * speedMultiplier).ToString();
+
+	}
+}

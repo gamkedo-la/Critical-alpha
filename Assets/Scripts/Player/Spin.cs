eb210e63320930a27cb564b0c70a80bc47f9dd19
TimWaskett <tim.waskett@gmail.com>
2016-06-20 13:43:24 +0100

    Added spinning propeller to the player Spad XIII

---
+++ Assets/Scripts/Player/Spin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField] float m_speed = 2000;
+
+	void Update()
+    {
+        transform.Rotate(Vector3.forward, m_speed * Time.deltaTime);
+	}
+}

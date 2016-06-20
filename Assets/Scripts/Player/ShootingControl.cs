ba4dc558571637e6f43bf0c4bfcf49f3acf05284
TimWaskett <tim.waskett@gmail.com>
2016-06-20 08:16:48 +0100

    Added muzzle flash to the Spad XIII gun firing

--- Assets/Scripts/Player/ShootingControl.cs
+++ Assets/Scripts/Player/ShootingControl.cs
@@ -6,17 +6,35 @@ public class ShootingControl : MonoBehaviour
     [SerializeField] BulletFlight m_bullet;
     [SerializeField] Transform[] m_bulletSpawnPoints;
     [SerializeField] float m_bulletCooldown = 0.15f;
+    [SerializeField] float m_muzzleFlashTime = 0.05f;
 
     private float m_timeSinceBulletFired;
     private FlyingControl m_flyingControlScript;
     private int m_spawnPointIndex;
     private int m_numSpawnPoints;
+    private WaitForSeconds m_muzzleFlashWait;
 
 
     void Awake()
     {
         m_flyingControlScript = GetComponent<FlyingControl>();
         m_numSpawnPoints = m_bulletSpawnPoints.Length;
+        m_muzzleFlashWait = new WaitForSeconds(m_muzzleFlashTime);
+    }
+
+
+    void Start()
+    {
+        for (int i = 0; i < m_bulletSpawnPoints.Length; i++)
+        {
+            var spawnPoint = m_bulletSpawnPoints[i];
+            
+            if (spawnPoint.childCount > 0)
+            {
+                var muzzleFlash = spawnPoint.GetChild(0);
+                muzzleFlash.gameObject.SetActive(false);
+            }              
+        }
     }
 
 
@@ -36,7 +54,7 @@ public class ShootingControl : MonoBehaviour
             bullet.transform.parent = transform;
 
             var spawnPoint = m_bulletSpawnPoints[m_spawnPointIndex];
-
+            
             bullet.transform.position = spawnPoint.position;
 
             bullet.transform.rotation = spawnPoint.rotation;
@@ -46,8 +64,24 @@ public class ShootingControl : MonoBehaviour
             else
                 bullet.SetInitialVelocity(Vector3.zero);
 
+            if (spawnPoint.childCount > 0)
+            {
+                var muzzleFlash = spawnPoint.GetChild(0);
+                muzzleFlash.gameObject.SetActive(true);
+
+                StartCoroutine(TurnOffQuad(muzzleFlash.gameObject));
+            }
+
             m_spawnPointIndex++;
-            m_spawnPointIndex = m_spawnPointIndex % m_numSpawnPoints;
+            m_spawnPointIndex = m_spawnPointIndex % m_numSpawnPoints;   
         }
+    }
+
+
+    private IEnumerator TurnOffQuad(GameObject muzzleFlashObject)
+    {
+        yield return m_muzzleFlashWait;
+
+        muzzleFlashObject.SetActive(false);
     }
 }

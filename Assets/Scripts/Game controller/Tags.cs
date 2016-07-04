1e535fa9900104110d369f6b21ec420c5cf74c8c
TimWaskett <tim.waskett@gmail.com>
2016-07-04 20:27:24 +0100

    Some adjustments to the alien prefab.

--- Assets/Scripts/Game controller/Tags.cs
+++ Assets/Scripts/Game controller/Tags.cs
@@ -9,4 +9,7 @@ public class Tags : MonoBehaviour
     public const string Cloud = "Cloud";
     public const string MapGenerator = "Map generator";
     public const string Bullet = "Bullet";
+    public const string EnemyAir = "Enemy Air";
+    public const string EnemyLand = "Enemy Land";
+    public const string EnemySea = "Enemy Sea";
 }

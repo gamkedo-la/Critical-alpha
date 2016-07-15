fb60676853d211a74a8d95a406def0500d6821ad
TimWaskett <tim.waskett@gmail.com>
2016-07-15 08:19:06 +0100

    Added lights to the jet exhausts, plus a few more value tweaks

--- Assets/Scripts/Game controller/SetUpCullingDistances.cs
+++ Assets/Scripts/Game controller/SetUpCullingDistances.cs
@@ -6,13 +6,13 @@ public class SetUpCullingDistances : MonoBehaviour
 	void Start() 
 	{
 		var camera = Camera.main;
-		float[] distances = new float[32];
+		//float[] distances = new float[32];
 
-		distances[9] = 1200;		// Small object
-		distances[10] = 1500;		// Medium object
+		//distances[9] = 1200;		// Small object
+		//distances[10] = 1500;		// Medium object
 		//distances[11] = 1500;		// Large object
 
-		camera.layerCullDistances = distances;
+		//camera.layerCullDistances = distances;
 		camera.layerCullSpherical = true;
 	}
 }

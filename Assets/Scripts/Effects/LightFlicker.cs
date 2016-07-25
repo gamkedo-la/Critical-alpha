94b721fb6d779a97ceb1d6b911f9a371daa196c3
TimWaskett <tim.waskett@gmail.com>
2016-07-25 21:34:46 +0100

    Fire material tweaks.

--- Assets/Scripts/Effects/LightFlicker.cs
+++ Assets/Scripts/Effects/LightFlicker.cs
@@ -5,11 +5,14 @@ using System.Collections;
 public class LightFlicker : MonoBehaviour
 {
     [SerializeField] Vector2 m_intensityMultiplierMinMax = new Vector2(0.8f, 1.2f);
-    [SerializeField] float m_flickerFrequency = 1f;
+    [SerializeField] float m_flickerFrequency = 4f;
+    [SerializeField] float m_lightFollowSpeed = 2f;
 
     private Light m_light;
     private float m_intensity;
     private float m_perlinX;
+    private Transform m_parent;
+    private Vector3 m_parentOffset;
 
     
     void Awake()
@@ -17,6 +20,9 @@ public class LightFlicker : MonoBehaviour
         m_light = GetComponent<Light>();
         m_intensity = m_light.intensity;
         m_perlinX = Random.Range(-100f, 100f);
+        m_parent = transform.parent;
+        m_parentOffset = transform.position - m_parent.position;
+        transform.parent = null;
     }
 	
 
@@ -24,5 +30,6 @@ public class LightFlicker : MonoBehaviour
     {
         float noise = Mathf.PerlinNoise(m_perlinX, Time.time * m_flickerFrequency);
         m_light.intensity = m_intensity * Mathf.Lerp(m_intensityMultiplierMinMax.x, m_intensityMultiplierMinMax.y, noise);
+        transform.position = Vector3.Lerp(transform.position, m_parent.position + m_parentOffset, Time.deltaTime * m_lightFollowSpeed);
 	}
 }
